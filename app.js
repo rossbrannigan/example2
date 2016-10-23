6762b2109cae01eea31a2e79ecbaa7a5294eaceb
Ross Brannigan <rossbrannigan@gmail.com>
2016-10-23 11:53:02 +0200

    Included basic .JS interactivity

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-app.js
+var main = function () {
+	"use strict";
+
+	$(".comment-input button").on("click", function (event) {
+		$(".comments").append("<p>this is a new comment</p>");
+});
+};
+$(document).ready(main);
